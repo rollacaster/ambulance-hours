e894fd4e7a119ca3677d0c6c9f3d5918c07752c9
Thomas Sojka <thsojka@web.de>
2020-10-26 12:37:08 +0100

    Add all interactive components

--- src/ambulance_hours/core.cljs
+++ src/ambulance_hours/core.cljs
@@ -7,12 +7,30 @@
    [cljs.reader :refer [read-string]]
    ["@expo/vector-icons" :as icons]))
 
-(defonce data (r/atom {"K3003" [{:date (new js/Date) :hours 2}
-                                {:date (new js/Date) :hours 2}
-                                {:date (new js/Date) :hours 2}]
-                       "S1609" [{:date (new js/Date) :hours 2}
-                                {:date (new js/Date) :hours 2}
-                                {:date (new js/Date) :hours 2}]}))
+(defonce data (r/atom {"K300391" [{:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}]
+                       "S160990" [{:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}]
+                       "A280388" [{:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}]
+                       "F280488" [{:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}]
+                       "L280488" [{:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}]
+                       "Q280488" [{:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}
+                                  {:date (new js/Date) :hours 2}]}))
+
+(defn button [{:keys [on-press]} children]
+  [:> rn/TouchableHighlight
+   {:style (tw "bg-orange-500 justify-center w-10 h-10 items-center rounded mb-4")
+    :on-press on-press}
+   children])
 
 (defn total-hours [data]
   (->> data
@@ -23,7 +41,7 @@
 
 (defn root []
   (-> (.getItem rn/AsyncStorage "data")
-      (.then read-string)
+      (.then (fn [loaded-data] (if loaded-data (read-string loaded-data) @data)))
       (.then #(reset! data %)))
   (fn []
     [:> rn/SafeAreaView
@@ -40,9 +58,53 @@
       {:style (tw "flex-1 pt-6 bg-white w-full")}
       [:> rn/View
        {:style (tw "items-center")}
-       [:> rn/Text {:style (tw "text-6xl font-extrabold tracking-wide  mb-4")} (total-hours @data)]]]
+       [:> rn/View
+        {:style (tw "mb-4 bg-orange-200 px-5 rounded-lg")}
+        [:> rn/Text {:style (tw "text-gray-900 text-6xl font-extrabold tracking-wide")} (total-hours @data)]]]
+      [:> rn/ScrollView
+       (map-indexed
+        (fn [idx [chiffre hours]]
+          [:> rn/View
+           {:key idx
+            :style (tw (str "flex-row px-6 py-6 justify-between " (when (even? idx) "bg-gray-300")))}
+           [:> rn/TouchableHighlight
+            {:on-press #()
+             :style (tw (str "px-2 justify-center items-center rounded-lg flex-row " (if (even? idx) "bg-gray-100" "bg-gray-700")))}
+            [:<>
+             [:> rn/Text
+              {:style (tw (str "text-2xl " (when (odd? idx) "text-white")))}
+              chiffre]
+             [:> icons/EvilIcons {:name "eye" :size 32 :color (if (even? idx) "black" "white")
+                                  :style (tw "mr-1")}]]]
+           [:> rn/View
+            {:style (tw "flex-row items-center pt-3")}
+            [button {:on-press #(prn "hi")}
+             [:> rn/Text {:style (tw "text-gray-900 text-3xl text-white")} "-"]]
+            [:> rn/View
+             [:> rn/Text
+              {:style (tw "text-2xl px-4 pb-3")}
+              (->> hours
+                   (map :hours)
+                   (reduce +))]]
+            [button {:on-press #(prn "hi")}
+             [:> rn/Text {:style (tw "text-gray-900 text-3xl text-white")} "+"]]]])
+        @data)]
+      [:> rn/TouchableHighlight
+       {:style (clj->js (merge {:bottom 0
+                                :right 0
+                                :shadowColor "#000",
+                                :shadowOffset {:width 0, :height 2},
+                                :shadowOpacity 0.25,
+                                :shadowRadius 3.84,
+                                :elevation 5}
+                               (js->clj (tw "absolute p-6"))))}
+       [:> rn/View
+        {:style (tw "bg-orange-400 rounded-full w-16 h-16 items-center justify-center")}
+        [:> rn/Text
+         {:style (tw "text-white text-5xl font-bold")}
+         "+"]]]]
      [:> rn/View
-      {:style (tw "bg-orange-400 w-full items-center pt-2")}
+      {:style (tw "bg-orange-400 w-full items-center py-2")}
       [:> rn/Text
        {:style (tw "text-white")}
        "made with 💖"]]]))
