47db011450ba9d437729d1cc5b2d8a9905bffe8a
Thomas Sojka <thsojka@web.de>
2022-05-26 15:23:25 +0200

    Fix input layout

--- src/fe/ambulance_hours/core.cljs
+++ src/fe/ambulance_hours/core.cljs
@@ -20,7 +20,7 @@
 (defn button [{:keys [class on-click secondary disabled]} children]
   [:button.justify-center.items-center.rounded.px-3.py-2
    {:class [(when-not disabled (if secondary "bg-gray-400" "bg-orange-400"))
-            (when disabled "bg-gray-200 opacity-50")
+            (when disabled "bg-gray-200 opacity-50 text-gray-400")
             class]
     :disabled disabled
     :on-click on-click}
@@ -228,15 +228,16 @@
       [:div.flex.px-6.py-4 {:class (when (odd? hour) " bg-gray-300")}
        [:div.flex.flex-col {:class "w-2/3"}
         [:span.text-lg.mb-1.font-bold (str "Stunde " hour)]
-        [:input.p-2.mr-4.rounded
-         {:type "datetime-local"
+        [:input.p-2.rounded
+         {:style {:width "90%"}
+          :type "datetime-local"
           :value (date-fns/format @updated-date "yyyy-MM-dd'T'HH:mm")
           :on-change (fn [^js e] (reset! updated-date (date-fns/parse (.-target.value e) "yyyy-MM-dd'T'HH:mm" (new js/Date))))}]]
-       [:div.flex.flex-col {:class "w-1/3"}
-        [button {:on-click #(on-save @updated-date) :class "mb-2 bg-gray-100" :disabled (= date @updated-date)}
-         [:span.text-white "Speichern"]]
+       [:div.flex.flex-col.text-white {:class "w-1/3"}
+        [button {:on-click #(on-save @updated-date) :class "mb-2" :disabled (= date @updated-date)}
+         "Speichern"]
         [button {:on-click on-remove :secondary true}
-         [:span..text-white "Löschen"]]]])))
+         "Löschen"]]])))
 
 (defn vec-remove
   "remove elem in coll"
